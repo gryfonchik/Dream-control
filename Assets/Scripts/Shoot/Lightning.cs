939d0e3d95300e9bddf8091fd3f49d6d2e11c9d2
Gryfonchik <kostya.platoch@mail.ru>
2024-11-12 12:06:15 +1000

    Atack

--- Assets/Scripts/Shoot/Lightning.cs
+++ Assets/Scripts/Shoot/Lightning.cs
@@ -8,7 +8,6 @@ public class Lightning : MonoBehaviour
     public LayerMask targetLayer;              // Слой целей (например, врагов)
     public int numPoints = 30;                 // Количество точек молнии
     public float lightningDuration = 0.1f;     // Время, в течение которого молния полностью видима
-    public ParticleSystem lightningParticles;  // Система частиц для молнии
     private Vector3[] lightningPoints;         // Массив точек молнии
 
     void Start()
@@ -74,8 +73,6 @@ public class Lightning : MonoBehaviour
             lineRenderer.positionCount = i + 1; // Увеличиваем количество точек
             lineRenderer.SetPosition(i, lightningPoints[i]); // Устанавливаем текущую точку
 
-            // Эмитируем частицы на текущей точке молнии
-            EmitParticlesAtPoint(lightningPoints[i]);
 
             yield return new WaitForSeconds(lightningDuration / numPoints); // Задержка между появлением точек
         }
@@ -85,14 +82,6 @@ public class Lightning : MonoBehaviour
 
         // Отключаем LineRenderer после того, как молния погасла
         lineRenderer.positionCount = 0;
-    }
-
-    private void EmitParticlesAtPoint(Vector3 position)
-    {
-        // Создаем и эмитируем частицы в заданной позиции
-        var emission = lightningParticles.emission;
-        lightningParticles.transform.position = position; // Перемещаем систему частиц в текущую точку молнии
-        lightningParticles.Emit(1); // Эмитируем одну частицу
     }
 }
 
